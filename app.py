3deee5f1022258319b372755ca238c8d7e65729b
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2020-03-19 15:01:29 -0700

    Make list comp to parse words, print out in color

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import api_key
+import pprint
+from termcolor import colored
 
 word = input('Word: ')
 
@@ -8,4 +10,8 @@ full_url = f'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{wor
 request = requests.get(full_url)
 json_ = request.json()
 
-print(json_)
+print(f'{word}')
+print(colored('\nSynonyms\n', 'green'))
+syn = [print(item['wd']) for item in json_[1]['def'][0]['sseq'][0][0][1]['syn_list'][0]]
+print(colored('\nAntonyms\n', 'red'))
+ant = [print(item['wd']) for item in json_[1]['def'][0]['sseq'][0][0][1]['ant_list'][0]]
