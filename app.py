84df20d643db4e3f068534a4d151e2ee471e5fce
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-10-26 00:25:18 -0700

    Make script a little better
    - Use functions for repeated behavior
    - Use more functions
    - Organize script
    - Add try except

--- app.py
+++ app.py
@@ -4,14 +4,34 @@ import api_key
 import pprint
 from termcolor import colored
 
-word = input('Word: ')
 
-full_url = f'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{word}?key={api_key.key}'
-request = requests.get(full_url)
-json_ = request.json()
+def get_words(word_json: dict, word_type: str) -> list:
+    """Return list of words based on word type e.g. 'syn_list'"""
+    try:
+        return [item['wd'] for item in word_json[1]['def'][0]['sseq'][0][0][1][word_type][0]]
+    except:
+        raise Exception("Sorry, word not found")
 
-print(f'{word}')
-print(colored('\nSynonyms\n', 'green'))
-syn = [print(item['wd']) for item in json_[1]['def'][0]['sseq'][0][0][1]['syn_list'][0]]
-print(colored('\nAntonyms\n', 'red'))
-ant = [print(item['wd']) for item in json_[1]['def'][0]['sseq'][0][0][1]['ant_list'][0]]
+def print_word_list(word_list: list):
+    """Goes through the words and prints them"""
+    for word in word_list:
+        print(word)
+
+if __name__ == "__main__":
+    word = input('Word: ')
+
+    word_url = f"references/thesaurus/json/{word}?key={api_key.key}"
+    full_url = f"https://www.dictionaryapi.com/api/v3/{word_url}"
+    request = requests.get(full_url)
+    json_ = request.json()
+
+    syn = get_words(json_, "syn_list")
+    ant = get_words(json_, "ant_list")
+
+    print(word)
+
+    print(colored('\nSynonyms\n', 'green'))
+    print(print_word_list(syn))
+
+    print(colored('\nAntonyms\n', 'red'))
+    print(print_word_list(ant))
